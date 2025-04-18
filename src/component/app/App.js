169eca9d6953689af479866204d0fb6cbcb4334a
Olga <Kuzma-InoverOlga@mail.ru>
2025-04-18 10:43:00 +0300

    change file before change webpack config

---
+++ src/component/app/App.js
@@ -0,0 +1,11 @@
+import Promo from "../promo/promo";
+
+function App() {
+    return (
+        <main className="App">
+            <Promo />
+        </main>
+    );
+}
+
+export default App;
---
+++ src/component/promo/promo.js
@@ -0,0 +1,9 @@
+import "./promo.scss";
+
+const Promo = () => {
+    <section>
+        <h1>Hello Olga</h1>
+    </section>;
+};
+
+export default Promo;
